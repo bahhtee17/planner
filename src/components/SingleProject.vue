8d4403d3a61089193caf5dfbff8c084c484bc7c3
Baxtiyor <baxtiyorshukurjanov36@gmail.com>
2021-08-15 20:13:29 +0500

    added new component

---
+++ src/components/SingleProject.vue
@@ -0,0 +1,18 @@
+<template>
+ <div class="container">
+     <div class="action">
+        <h3>{{project.title}}</h3>
+     </div>
+ </div>
+</template>
+
+<script>
+export default {
+    props: ['project']
+
+}
+</script>
+
+<style>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,7 +2,7 @@
 <div class="home">
   <div v-if="projects.length">
     <div v-for="project in projects" :key="project.id">
-        <p>{{project.title}}</p>
+       <single-project :project="project"></single-project>
     </div>
   </div>
 
@@ -10,12 +10,12 @@
 </template>
 
 <script>
-
+import SingleProject from '../components/SingleProject.vue'
 
 export default {
   name: 'Home',
   components: {
-
+     SingleProject
   },
   data(){
     return{
