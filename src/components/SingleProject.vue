f93e6e2d5df046c5cb7482ffd93afeae533e353f
Baxtiyor <baxtiyorshukurjanov36@gmail.com>
2021-08-15 23:29:11 +0500

    added bg img

--- src/components/SingleProject.vue
+++ src/components/SingleProject.vue
@@ -1,7 +1,11 @@
 <template>
  <div class="container">
-     <div class="action">
+     <div class="title">
         <h3>{{project.title}}</h3>
+     </div>
+
+     <div class="details">
+        <p>{{project.details}}</p>
      </div>
  </div>
 </template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,11 @@
 <template>
 <div class="home">
+
+  <div class="header-bg">
+    <h1 class="header">Planner</h1>
+    <p class="sub-header">every day we change lifePlanner</p>
+  </div>
+
   <div v-if="projects.length">
     <div v-for="project in projects" :key="project.id">
        <single-project :project="project"></single-project>
@@ -31,3 +37,28 @@ export default {
   }
 }
 </script>
+<style>
+body{
+  margin: 0px;
+  padding: 0px;
+  background-color: beige;
+  font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+}
+
+.header-bg{
+
+  height: 400px;
+  background-image: linear-gradient(to right,rgb(202, 244, 255, 0.4), rgb(96, 137, 252, 0.7)),
+  url(../../public/photos/header-last.jpg);
+  background-size: cover;
+  clip-path: polygon(0 0, 100% 0, 100% 100%, 10% 89%);
+
+
+
+}
+
+
+
+
+
+</style>
