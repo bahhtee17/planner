79dadc98d98bc834aa3e684bf1a46ebced5232eb
Baxtiyor <baxtiyorshukurjanov36@gmail.com>
2021-08-16 23:41:09 +0500

    Collection is styled and created method openIt

--- src/components/SingleProject.vue
+++ src/components/SingleProject.vue
@@ -1,10 +1,10 @@
 <template>
- <div class="container">
+ <div class="container" @click="openIt">
      <div class="title">
         <h3>{{project.title}}</h3>
      </div>
 
-     <div class="details">
+     <div v-if="isOpen" class="details">
         <p>{{project.details}}</p>
      </div>
  </div>
@@ -12,11 +12,40 @@
 
 <script>
 export default {
-    props: ['project']
+    props: ['project'],
+
+    data(){
+        return{
+            isOpen: false
+        }
+    },
+
+    methods: {
+        openIt(){
+            this.isOpen = !this.isOpen
+        }
+    }
 
 }
 </script>
 
 <style>
+  .container{
+      display: inline-block;
+      width: 50vh;
+      padding: 5px;
+       margin-bottom: 10px;
+      background-color: rgb(240, 240, 65);
+      margin-left: 40%;
+      color: #444;
+      clip-path: polygon(0 0, 100% 0, 100% 100%, 0 77%);
+  }
 
+  .title{
+      margin-left: 50px;
+  }
+
+  .details{
+      margin-left: 55px;
+  }
 </style>
