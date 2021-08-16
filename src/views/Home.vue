c111243b9b0003afcae35f2b5d683ec15be64966
Baxtiyor <baxtiyorshukurjanov36@gmail.com>
2021-08-16 23:10:52 +0500

    header is styled

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,7 +3,7 @@
 
   <div class="header-bg">
     <h1 class="header">Planner</h1>
-    <p class="sub-header">every day we change lifePlanner</p>
+    <p class="sub-header">every day we change life</p>
   </div>
 
   <div v-if="projects.length">
@@ -47,18 +47,32 @@ body{
 
 .header-bg{
 
-  height: 400px;
-  background-image: linear-gradient(to right,rgb(202, 244, 255, 0.4), rgb(96, 137, 252, 0.7)),
+  height: 50vh;
+  background-position: top;
+  background-image: linear-gradient(to right,rgba(218, 255, 11, 0.4), rgba(255, 251, 5, 0.74)),
   url(../../public/photos/header-last.jpg);
   background-size: cover;
   clip-path: polygon(0 0, 100% 0, 100% 100%, 10% 89%);
-
-
+  color: #fff;
+  text-transform: uppercase;
+  left: 50%;
+  top: 50%;
 
 }
 
+.header{
+font-size: 60px;
+font-weight: 400;
+letter-spacing: 35px;
+transform: translate(38%, 50%);
+}
 
-
-
+.sub-header{
+  display: block;
+  font-size: 15px;
+  font-weight: 400;
+  letter-spacing: 7.5px;
+  transform: translate(41%, 50%);
+}
 
 </style>
